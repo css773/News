9bf7e6ec08fa10f5ec40ed4efe5d27a2b886a0c9
shuo <jaspercui@hotmail.com>
2020-01-26 17:37:50 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      article: []
+    };
+  }
+  componentDidMount() {
+    const endPoint = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c43c202b0d954204b0d7221151ecce26`;
+    fetch(endPoint)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          article: data.articles
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.article.map((item, index) => {
+          return (
+            <div key={index}>
+              <h2 style={{ textAlign: "left" }}>{item.title}</h2>
+              <b>{item.Author}</b>
+              <img src={item.urlToImage} alt="" style={{ width: "50vw" }} />
+              <p>{item.content}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
